d4030b218eff13f8726fe13cb7023c64ae2a17f4
Fr3nk <bugugnolifrancesco@gmail.com>
2024-08-30 16:23:38 +1000

    new TODO comment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./styles.css";
 import TasksContainer from "./components/TasksContainer.js";
 
+// TODO - Try to understand which is the best way to pass the selected date and the selected tag to the next created list task
 //TODO - Fix, style and set in the proper way the Date.js component
 //TODO - Fix, style and set in the proper way the Tag.js component
 //TODO - Create an option to select the Task's Project filter by date/tag for the tasks
